334c0f7101dc27edf935c6d9b09341bef3a17e32
Philipp Schardt <philipp.schardt@ergosign.de>
2016-09-23 10:05:07 +0200

    google analytics commentar fixed

--- src/index.php
+++ src/index.php
@@ -71,7 +71,7 @@ if ($this->params->get('googleFont'))
 					  name="debug"
 					  style="none"/>
 	</body>
-	// google analytics
+<!--	google analytics-->
 	<script>
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
